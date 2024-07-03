d85b1d94dabff22aff55d2abfec8c819b5e1dbe8
Adriasu <adriana.suarez@students.gammatech.school>
2024-07-04 00:33:16 +0200

    unificado el carrito con main y shop

--- js/main.js
+++ js/main.js
@@ -41,9 +41,105 @@ bag.addEventListener("click", () => {
     : (cart.style.display = "flex");
 });
 
-const arrayCart = [];
+const lsArray = JSON.parse(localStorage.getItem("arrayCart"));
+const totalPriceLs = JSON.parse(localStorage.getItem("totalPrice"));
+const totalOfProductsLs = JSON.parse(localStorage.getItem("totalProducts"));
+
+let arrayCart = [];
 let total = 0;
 let totalOfProducts = 0;
+
+if (lsArray !== null) {
+  arrayCart = lsArray;
+}
+
+if (totalPriceLs !== null) {
+  total = totalPriceLs;
+  totalPrice.innerHTML = `Check-Out: ${total.toFixed(2)} €`;
+}
+
+if (totalOfProductsLs !== null) {
+  totalOfProducts = totalOfProductsLs;
+  sumProducts.innerHTML = totalOfProducts;
+}
+
+arrayCart.forEach((selectedProduct) => {
+  const card = document.createElement("section");
+  const img = document.createElement("img");
+  const textContain = document.createElement("div");
+  const textName = document.createElement("p");
+  const textPrice = document.createElement("p");
+  const containCounterBtn = document.createElement("div");
+  const counter = document.createElement("p");
+  const btnAdd = document.createElement("img");
+  const btnSubtract = document.createElement("img");
+
+  containSelectedProducts.appendChild(card);
+  card.appendChild(img);
+  card.appendChild(textContain);
+  textContain.appendChild(textName);
+  textContain.appendChild(textPrice);
+  card.appendChild(containCounterBtn);
+  containCounterBtn.appendChild(btnAdd);
+  containCounterBtn.appendChild(counter);
+  containCounterBtn.appendChild(btnSubtract);
+
+  card.className = "cardSelected";
+  textContain.className = "textContain";
+  containCounterBtn.className = "containCounterBtn";
+  img.className = "imgCardSelected";
+  textName.className = "textNameCardSelected";
+  btnAdd.className = "btnAdd";
+  btnSubtract.className = "btnSubtract";
+
+  img.src = selectedProduct.img;
+  textName.innerHTML = selectedProduct.nameProduct;
+  textPrice.innerHTML = `${selectedProduct.price.toFixed(2)} €`;
+  btnAdd.src = "/assets/images/heroicons-outline_plus-sm.png";
+  counter.innerHTML = selectedProduct.count;
+  btnSubtract.src = "/assets/images/heroicons-outline_minus-sm.png";
+
+  btnAdd.addEventListener("click", () => {
+    totalOfProducts++;
+    sumProducts.innerHTML = totalOfProducts;
+    localStorage.setItem("totalProducts", JSON.stringify(totalOfProducts));
+
+    selectedProduct.count++;
+    counter.innerHTML = selectedProduct.count;
+
+    total += selectedProduct.price;
+    totalPrice.innerHTML = `Check-Out: ${total.toFixed(2)} €`;
+    localStorage.setItem("totalPrice", JSON.stringify(total));
+  });
+
+  btnSubtract.addEventListener("click", (event) => {
+    if (selectedProduct.count > 1) {
+      selectedProduct.count--;
+      counter.innerHTML = selectedProduct.count;
+    } else {
+      event.target.parentElement.parentElement.remove();
+      const deleteProduct = arrayCart.findIndex((product) => {
+        return product.id === selectedProduct.id;
+      });
+      arrayCart.splice(deleteProduct, 1);
+      localStorage.setItem("arrayCart", JSON.stringify(arrayCart));
+    }
+    totalOfProducts--;
+    sumProducts.innerHTML = totalOfProducts;
+    localStorage.setItem("totalProducts", JSON.stringify(totalOfProducts));
+
+    total -= selectedProduct.price;
+    totalPrice.innerHTML = `Check-Out: ${total.toFixed(2)} €`;
+    localStorage.setItem("totalPrice", JSON.stringify(total));
+
+    hideCart(containSelectedProducts);
+    cartEmpty(arrayCart, containSelectedProducts);
+  });
+});
+
+counterBag(totalOfProducts);
+
+//---------------------------------------------------------------------------//
 
 productsCards.slice(0, 4).forEach((card) => {
   const cardNewsWrapperProduct = document.createElement("div");
@@ -88,9 +184,11 @@ productsCards.slice(0, 4).forEach((card) => {
 
     totalOfProducts += productSelected.count;
     sumProducts.innerHTML = totalOfProducts;
+    localStorage.setItem("totalProducts", JSON.stringify(totalOfProducts))
 
     total += productSelected.count * productSelected.price;
     totalPrice.innerHTML = `Check-Out: ${total.toFixed(2)} €`;
+    localStorage.setItem("totalPrice", JSON.stringify(total))
 
     productCount(productSelected, arrayCart);
     localStorage.setItem("arrayCart", JSON.stringify(arrayCart));
@@ -136,12 +234,14 @@ productsCards.slice(0, 4).forEach((card) => {
       btnAdd.addEventListener("click", () => {
         totalOfProducts++;
         sumProducts.innerHTML = totalOfProducts;
+        localStorage.setItem("totalProducts", JSON.stringify(totalOfProducts))
 
         selectedProduct.count++;
         counter.innerHTML = selectedProduct.count;
 
         total += selectedProduct.price;
         totalPrice.innerHTML = `Check-Out: ${total.toFixed(2)} €`;
+        localStorage.setItem("totalPrice", JSON.stringify(total))
       });
 
       btnSubtract.addEventListener("click", (event) => {
@@ -158,9 +258,11 @@ productsCards.slice(0, 4).forEach((card) => {
         }
         totalOfProducts -= productSelected.count;
         sumProducts.innerHTML = totalOfProducts;
+        localStorage.setItem("totalProducts", JSON.stringify(totalOfProducts))
 
         total -= selectedProduct.price;
         totalPrice.innerHTML = `Check-Out: ${total.toFixed(2)} €`;
+        localStorage.setItem("totalPrice", JSON.stringify(total))
 
         hideCart(containSelectedProducts);
         cartEmpty(arrayCart, containSelectedProducts);
--- js/shop2.js
+++ js/shop2.js
@@ -55,9 +55,105 @@ bag.addEventListener("click", () => {
     : (cart.style.display = "flex");
 });
 
-const arrayCart = [];
+const lsArray = JSON.parse(localStorage.getItem("arrayCart"));
+const totalPriceLs = JSON.parse(localStorage.getItem("totalPrice"));
+const totalOfProductsLs = JSON.parse(localStorage.getItem("totalProducts"));
+
+let arrayCart = [];
 let total = 0;
 let totalOfProducts = 0;
+
+if (lsArray !== null) {
+  arrayCart = lsArray;
+}
+
+if (totalPriceLs !== null) {
+  total = totalPriceLs;
+  totalPrice.innerHTML = `Check-Out: ${total.toFixed(2)} €`;
+}
+
+if (totalOfProductsLs !== null) {
+  totalOfProducts = totalOfProductsLs;
+  sumProducts.innerHTML = totalOfProducts;
+}
+
+arrayCart.forEach((selectedProduct) => {
+  const card = document.createElement("section");
+  const img = document.createElement("img");
+  const textContain = document.createElement("div");
+  const textName = document.createElement("p");
+  const textPrice = document.createElement("p");
+  const containCounterBtn = document.createElement("div");
+  const counter = document.createElement("p");
+  const btnAdd = document.createElement("img");
+  const btnSubtract = document.createElement("img");
+
+  containSelectedProducts.appendChild(card);
+  card.appendChild(img);
+  card.appendChild(textContain);
+  textContain.appendChild(textName);
+  textContain.appendChild(textPrice);
+  card.appendChild(containCounterBtn);
+  containCounterBtn.appendChild(btnAdd);
+  containCounterBtn.appendChild(counter);
+  containCounterBtn.appendChild(btnSubtract);
+
+  card.className = "cardSelected";
+  textContain.className = "textContain";
+  containCounterBtn.className = "containCounterBtn";
+  img.className = "imgCardSelected";
+  textName.className = "textNameCardSelected";
+  btnAdd.className = "btnAdd";
+  btnSubtract.className = "btnSubtract";
+
+  img.src = selectedProduct.img;
+  textName.innerHTML = selectedProduct.nameProduct;
+  textPrice.innerHTML = `${selectedProduct.price.toFixed(2)} €`;
+  btnAdd.src = "/assets/images/heroicons-outline_plus-sm.png";
+  counter.innerHTML = selectedProduct.count;
+  btnSubtract.src = "/assets/images/heroicons-outline_minus-sm.png";
+
+  btnAdd.addEventListener("click", () => {
+    totalOfProducts++;
+    sumProducts.innerHTML = totalOfProducts;
+    localStorage.setItem("totalProducts", JSON.stringify(totalOfProducts));
+
+    selectedProduct.count++;
+    counter.innerHTML = selectedProduct.count;
+
+    total += selectedProduct.price;
+    totalPrice.innerHTML = `Check-Out: ${total.toFixed(2)} €`;
+    localStorage.setItem("totalPrice", JSON.stringify(total));
+  });
+
+  btnSubtract.addEventListener("click", (event) => {
+    if (selectedProduct.count > 1) {
+      selectedProduct.count--;
+      counter.innerHTML = selectedProduct.count;
+    } else {
+      event.target.parentElement.parentElement.remove();
+      const deleteProduct = arrayCart.findIndex((product) => {
+        return product.id === selectedProduct.id;
+      });
+      arrayCart.splice(deleteProduct, 1);
+      localStorage.setItem("arrayCart", JSON.stringify(arrayCart));
+    }
+    totalOfProducts--;
+    sumProducts.innerHTML = totalOfProducts;
+    localStorage.setItem("totalProducts", JSON.stringify(totalOfProducts));
+
+    total -= selectedProduct.price;
+    totalPrice.innerHTML = `Check-Out: ${total.toFixed(2)} €`;
+    localStorage.setItem("totalPrice", JSON.stringify(total));
+
+    hideCart(containSelectedProducts);
+    cartEmpty(arrayCart, containSelectedProducts);
+  });
+});
+
+counterBag(totalOfProducts);
+
+//-------------------------------------------------------------------------//
 
 productsCards.forEach((card) => {
   const cardNewsWrapperProduct = document.createElement("div");
@@ -102,13 +198,15 @@ productsCards.forEach((card) => {
 
     totalOfProducts += productSelected.count;
     sumProducts.innerHTML = totalOfProducts;
+    localStorage.setItem("totalProducts", JSON.stringify(totalOfProducts));
 
     total += productSelected.count * productSelected.price;
     totalPrice.innerHTML = `Check-Out: ${total.toFixed(2)} €`;
+    localStorage.setItem("totalPrice", JSON.stringify(total));
 
     productCount(productSelected, arrayCart);
     localStorage.setItem("arrayCart", JSON.stringify(arrayCart));
-    const arrayCartLS = JSON.parse(localStorage.getItem("arrayCart"))
+    const arrayCartLS = JSON.parse(localStorage.getItem("arrayCart"));
     containSelectedProducts.innerHTML = "";
 
     arrayCartLS.forEach((selectedProduct) => {
@@ -150,12 +248,15 @@ productsCards.forEach((card) => {
       btnAdd.addEventListener("click", () => {
         totalOfProducts++;
         sumProducts.innerHTML = totalOfProducts;
+        localStorage.setItem("totalProducts", JSON.stringify(totalOfProducts));
 
         selectedProduct.count++;
         counter.innerHTML = selectedProduct.count;
 
         total += selectedProduct.price;
         totalPrice.innerHTML = `Check-Out: ${total.toFixed(2)} €`;
+        localStorage.setItem("totalPrice", JSON.stringify(total));
+        console.log(total);
       });
 
       btnSubtract.addEventListener("click", (event) => {
@@ -170,11 +271,14 @@ productsCards.forEach((card) => {
           arrayCart.splice(deleteProduct, 1);
           localStorage.setItem("arrayCart", JSON.stringify(arrayCart));
         }
-        totalOfProducts -= productSelected.count;
+        totalOfProducts--;
         sumProducts.innerHTML = totalOfProducts;
+        localStorage.setItem("totalProducts", JSON.stringify(totalOfProducts));
 
         total -= selectedProduct.price;
         totalPrice.innerHTML = `Check-Out: ${total.toFixed(2)} €`;
+        localStorage.setItem("totalPrice", JSON.stringify(total));
+        console.log(total);
 
         hideCart(containSelectedProducts);
         cartEmpty(arrayCart, containSelectedProducts);
@@ -197,6 +301,6 @@ btnClear.addEventListener("click", () => {
   total = 0;
   totalPrice.innerHTML = "Check-Out: 0.00 €";
   counterBag(totalOfProducts);
-  localStorage.clear()
+  localStorage.clear();
   localStorage.setItem("arrayCart", JSON.stringify(arrayCart));
 });
